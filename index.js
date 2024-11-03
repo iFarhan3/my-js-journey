3256e5122e6894a007ed27c498eb4361fc818199
Farhan <zsmax7596@gmail.com>
2024-11-03 16:49:29 +0500

    first try to push

---
+++ index.js
@@ -0,0 +1,10 @@
+let person = ['farhan', 'good', 'honest' , 'age'];
+person[3] = '22';
+
+console.log(person);
+function greet (lastNme , age , car){
+    console.log('hello' + ' ' + lastNme + ' ' + age + ' ' + car);
+}
+
+
+greet('farhan' , '25' , 'bmw');
