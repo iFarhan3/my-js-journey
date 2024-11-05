c81e49dd277767d10f8b9af001b59efced1d6533
Farhan <140542134+iFarhan3@users.noreply.github.com>
2024-11-05 14:25:33 +0500

    just upload
    upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*let person = ['farhan', 'good', 'honest' , 'age']
+person[3] = '22'
+
+console.log(person);*/
+
+function greet (lastNme , age , car){
+    console.log('hello' + ' ' + lastNme + ' ' + age )
+}
+
+ 
+greet('farhan' , '25' , 'bmw')
---
+++ index1.js
@@ -0,0 +1,16 @@
+const accountId = (45678)
+let favCar = ('BMW')
+let girl = ('purple')
+favCar = ('Benz')
+/*let farhan = ('Farhan is GREAT!')
+let age = (22)*/
+
+
+function intro (nme, age){
+    console.log(nme + ' ' + age + ' ' + accountId + ' ' + favCar)
+}
+
+intro ('farhan' , '22') 
+
+// console.log(accountId)
+console.table([accountId, favCar, girl])
