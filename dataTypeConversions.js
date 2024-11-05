8db75eb793724c19d37348e8017d7df24db16042
Farhan <zsmax7596@gmail.com>
2024-11-05 18:22:55 +0500

    dataConversion

---
+++ dataTypeConversions.js
@@ -0,0 +1,34 @@
+// converting string into number
+
+//let score = '33'
+//let score = 'fars'
+//let score = null
+//let score = undefined
+//let score = 'fars'
+let nom = Number(score)
+
+console.log(typeof nom);
+//console.log(typeof score);
+console.log(nom)
+
+
+//converting number into string
+
+/*let line = 56
+let string = String(line)
+
+//console.log(typeof line);
+console.log(typeof string);*/
+
+//conversion in boolean
+/*
+//let farhan = 0
+//let farhan = 1
+//let farhan = null
+//let farhan = undefined
+//let farhan = 'farhan'
+let boolean = Boolean(farhan)
+
+//console.log(typeof farhan)
+console.log(typeof boolean)
+console.log(boolean)*/
