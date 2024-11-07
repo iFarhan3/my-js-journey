d341b11a766157d84ce21b6c629836f7d1fb8c46
Farhan <zsmax7596@gmail.com>
2024-11-07 15:08:41 +0500

    new learning in ops03

--- operations03.js
+++ operations03.js
@@ -21,8 +21,39 @@ failed*/
 
 //postfix operators value++,-- they don't change the value at first
 
-let nsp = 10;
-let newNsp = nsp++;
+// let nsp = 10;
+// let newNsp = nsp++;
 
-console.log(nsp);
-console.log(newNsp);
+// console.log(nsp);
+// console.log(newNsp);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// type of memmories
+//1. Stack , 2. Heap 
+
+//Stack> Primitive values(string,numbers,boolean etc)
+// in stack memmory we get copy of variables, that means it doesn't effect the original var
+
+// let phone = 'Google';
+// let otherPhone = phone;
+// otherPhone = 'Apple';
+
+
+// console.log(phone);
+// console.log(otherPhone);
+
+//Heap> non-primitive values(object, array, functions)
+//in heap memmory, if we change the value of variable it will effect the original value too.
+
+let phone ={
+    brand : 'google',
+    model : '4xl',
+};
+
+let otherPhone = phone;
+otherPhone.model = '6pro' 
+
+console.log(phone)
+ console.log(otherPhone.model)
+//console.log(otherPhone)
